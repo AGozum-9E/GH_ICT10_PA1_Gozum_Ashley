419a703da82d09838cb450210c472b5e99f0fb1b
AGozum-10E <gozum.ac@obmontessori.edu.ph>
2025-11-13 08:53:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Working with Sets
+from pyscript import display
+
+art_club = {'Aurelia','Macy', 'Maica'}
+vb_varsity = {'Adrianna', 'Macy', 'Hikari'}
+both_clubs = {'Macy'}
+
+display(f'art club: {art_club}', target='output')
+display(f'volleyball club: {vb_varsity}', target='output')
+display(f'both clubs: {both_clubs}', target='output')
